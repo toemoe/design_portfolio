886d53e2ce4d45cfa4ce8b00a992c789e79ce7b9
strangka <strangka@student.21-school.ru>
2025-08-03 21:27:15 +0700

    change selected

--- my-app/src/components/ModalImage.tsx
+++ my-app/src/components/ModalImage.tsx
@@ -1,4 +1,5 @@
 import '../styles/App.css';
+import { useState } from 'react';
 
 interface ModalProps {
     isOpen: boolean;
@@ -8,11 +9,18 @@ interface ModalProps {
 }
 
 const ModalImage: React.FC<ModalProps> = ({ imageUrl, alt, isOpen, onClose }) => {
+    const [isZoomed, setIsZoomed] = useState(false);
+
+    const handleImageClick = () => setIsZoomed(!isZoomed);
+    const zoomLevel = isZoomed ? 1.3 : 1;
     if (!isOpen) return null;
 
     return (
         <div className='ModalOverlay' onClick={onClose}>
-                 <img src={imageUrl} alt={alt} className="modalImage" onClick={e => e.stopPropagation()}/>
+            <div className='ModalContent' onClick={e => e.stopPropagation()}>
+                 <img src={imageUrl} alt={alt} className="modalImage"
+                 onClick={handleImageClick} style={{ transform: `scale(${zoomLevel})`}}/>
+            </div>
         </div>
     );
 };
